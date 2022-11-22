04910b50f1f5391510ae128cc43e92caf7837165
dbwofla <mynok2714@gmail.com>
2022-11-22 23:46:59 +0900

    feat/DB

---
+++ db.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+user_name = "root"
+user_pwd = ""
+db_host = "127.0.0.1"
+db_name = "project01"
